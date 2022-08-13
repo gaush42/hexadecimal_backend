25bfd61eda74085142dfe7b7af7152facab76bf7
Gulam Gaush <mohammadgulamgaush@gmail.com>
2022-08-13 10:32:49 +0530

    .

--- routes/details.js
+++ routes/details.js
@@ -2,9 +2,18 @@ const router = require('express').Router();
 const fetch = require('node-fetch');
 
 router.route('/').get( async (req, res) => {
-    const api_url = 'https://jsonplaceholder.typicode.com/users'
-    const rest = await fetch(api_url)
-    const data = await rest.json();
-    res.json(data)
+    try{
+        const api_url = 'https://jsonplaceholder.typicode.com/users'
+        const rest = await fetch(api_url)
+        const data = await rest.json();
+        return res.status(200).json({
+            success: true, data
+        })
+    }catch (e){
+        return res.status(500).json({
+            message: 'Error', error: e.message
+        })
+    }
+    
 })
 module.exports = router;
